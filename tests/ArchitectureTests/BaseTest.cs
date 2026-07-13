88522dee367d0cd03c1fa6fbc8a402775e8246bd
Paul Custance <paul.custance@education.gov.uk>
2026-07-13 22:33:53 +0100

    Add an assembly marker for domain assembly

--- tests/ArchitectureTests/BaseTest.cs
+++ tests/ArchitectureTests/BaseTest.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using Domain;
 
 namespace ArchitectureTests;
 
 public abstract class BaseTest
 {
-    protected static readonly Assembly DomainAssembly = typeof(Readme).Assembly;
+    protected static readonly Assembly DomainAssembly = typeof(Domain.AssemblyMarker).Assembly;
     protected static readonly Assembly ApplicationAssembly = typeof(Application.DependencyInjection).Assembly;
     protected static readonly Assembly InfrastructureAssembly = typeof(Infrastructure.DependencyInjection).Assembly;
     protected static readonly Assembly PresentationAssembly = typeof(Web.Api.DependencyInjection).Assembly;
