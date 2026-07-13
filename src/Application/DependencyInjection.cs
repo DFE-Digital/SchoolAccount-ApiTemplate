5f2703789350385742ebf3a737e43e84dc42b1a4
Paul Custance <paul.custance@education.gov.uk>
2026-07-13 22:56:01 +0100

    Remove further write side classes

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,8 +1,6 @@
 ﻿using Application.Abstractions.Behaviors;
 using Application.Abstractions.Messaging;
-using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
-using SharedKernel;
 
 namespace Application;
 
@@ -22,13 +20,6 @@ public static class DependencyInjection
                 .WithScopedLifetime());
 
         services.TryDecorate(typeof(IQueryHandler<,>), typeof(LoggingDecorator.QueryHandler<,>));
-
-        services.Scan(scan => scan.FromAssembliesOf(typeof(DependencyInjection))
-            .AddClasses(classes => classes.AssignableTo(typeof(IDomainEventHandler<>)), publicOnly: false)
-            .AsImplementedInterfaces()
-            .WithScopedLifetime());
-
-        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
 
         return services;
     }
