7b1e6648ab2ad3d47c3656d0773558f2a80c6c4b
Paul Custance <paul.custance@education.gov.uk>
2026-07-14 11:58:20 +0100

    Remove fix for nested classes

--- src/Web.Api/DependencyInjection.cs
+++ src/Web.Api/DependencyInjection.cs
@@ -6,8 +6,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
-        services.AddOpenApi(static o =>
-            o.CreateSchemaReferenceId = t => t.Type.FullName?.Replace('+', '-'));
+        services.AddOpenApi();
         services.AddExceptionHandler<GlobalExceptionHandler>();
         services.AddProblemDetails();
 
