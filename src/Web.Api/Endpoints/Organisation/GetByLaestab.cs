e360d9eb65b5d9a1607e7353061b2fd4a3f96736
Paul Custance <paul.custance@education.gov.uk>
2026-07-16 14:41:39 +0100

    Add a request class for fetching orgs

--- src/Web.Api/Endpoints/Organisation/GetByLaestab.cs
+++ src/Web.Api/Endpoints/Organisation/GetByLaestab.cs
@@ -1,6 +1,8 @@
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using Application.Abstractions.Messaging;
 using Application.Organisation.GetByLaestab;
+using Microsoft.AspNetCore.Mvc;
 using SharedKernel;
 using Web.Api.Extensions;
 using Web.Api.Infrastructure;
@@ -10,14 +12,16 @@ namespace Web.Api.Endpoints.Organisation;
 [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.AllConstructors)]
 internal sealed class GetByLaestab : IEndpoint
 {
+    public sealed record Request([FromRoute] int Laestab);
+    
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("organisation/{laestab:regex(^\\d{{7}}$)}", async (
-                int laestab,
+        app.MapGet("organisation/{laestab}", async (
+                [AsParameters] Request request,
                 IQueryHandler<GetOrganisationByLaestabQuery, OrganisationResponse> handler,
                 CancellationToken cancellationToken) =>
             {
-                var query = new GetOrganisationByLaestabQuery(laestab);
+                var query = new GetOrganisationByLaestabQuery(request.Laestab);
 
                 Result<OrganisationResponse> result = await handler.Handle(query, cancellationToken);
 
