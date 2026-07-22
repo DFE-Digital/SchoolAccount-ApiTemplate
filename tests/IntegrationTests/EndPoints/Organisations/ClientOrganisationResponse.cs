35277e8e4673700da95740884ccfaf1447917db5
markharrop <mark.harrop@education.gov.uk>
2026-07-22 09:27:53 +0100

    fix integration test, improve name add asserts Begin unhappy path test

---
+++ tests/IntegrationTests/EndPoints/Organisations/ClientOrganisationResponse.cs
@@ -0,0 +1,3 @@
+namespace IntegrationTests.EndPoints.Organisations;
+
+public record ClientOrganisationResponse(string LocalAuthorityCode, string EstablishmentNo, string Status);
--- tests/IntegrationTests/EndPoints/Organisations/LaestabApiTests.cs
+++ tests/IntegrationTests/EndPoints/Organisations/LaestabApiTests.cs
@@ -1,5 +1,6 @@
 using Application.Abstractions.Messaging;
 using Application.Organisations.GetByLaestab;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using NSubstitute;
@@ -10,38 +11,70 @@ namespace IntegrationTests.EndPoints.Organisations;
 
 public class LaestabApiTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly HttpClient client;
+    private readonly HttpClient _client;
 
     private readonly IQueryHandler<GetOrganisationByLaestabQuery, OrganisationResponse> _organisationHandler =
         Substitute.For<IQueryHandler<GetOrganisationByLaestabQuery, OrganisationResponse>>();
 
     public LaestabApiTests(WebApplicationFactory<Program> factory)
     {
-        client = factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(services =>
-            services.AddScoped<IQueryHandler<GetOrganisationByLaestabQuery, OrganisationResponse>>(_ =>
-                _organisationHandler))).CreateClient();
+        _client = factory.WithWebHostBuilder(builder =>
+            builder.ConfigureTestServices(services =>
+                services.AddScoped<IQueryHandler<GetOrganisationByLaestabQuery, OrganisationResponse>>(_ =>
+                    _organisationHandler)
+            )
+        ).CreateClient();
     }
 
     [Fact]
-    public async Task GetOrganisationByLaestab_Returns_OrganisationResponse()
+    public async Task
+        Organisations_endpoint_should_return_localAuthorityCode_EstablishmentNo_and_status_as_a_string_for_valid_laestab()
     {
         // Arrange
-        string laestab = "1234567";
-        var expectedResponse = new OrganisationResponse
+        string localAuthorityCode = "987";
+        string establishmentNo = "6543";
+        string laestab = localAuthorityCode + establishmentNo;
+
+        var stubbedOrganisationResponse = new OrganisationResponse
         {
-            LocalAuthorityCode = "Test Organisation",
-            EstablishmentNo = "1234567",
-            Status = OrgStatus.Open
+            LocalAuthorityCode = localAuthorityCode,
+            EstablishmentNo = establishmentNo,
+            Status = OrgStatus.Closed
         };
 
-        _organisationHandler.Handle(Arg.Any<GetOrganisationByLaestabQuery>(), CancellationToken.None)
-            .Returns(Result.Success(expectedResponse));
+        _organisationHandler.Handle(Arg.Any<GetOrganisationByLaestabQuery>(), Arg.Any<CancellationToken>())
+            .Returns(Result.Success(stubbedOrganisationResponse));
 
         // Act
         HttpResponseMessage response =
-            await client.GetAsync($"/organisations/{laestab}", CancellationToken.None);
+            await _client.GetAsync($"/organisations/{laestab}", CancellationToken.None);
 
         // Assert
         response.StatusCode.ShouldBe(System.Net.HttpStatusCode.OK);
+        ClientOrganisationResponse? result =
+            await response.Content.ReadFromJsonAsync<ClientOrganisationResponse>(CancellationToken.None);
+        result.ShouldNotBeNull();
+        result.EstablishmentNo.ShouldBe(stubbedOrganisationResponse.EstablishmentNo);
+        result.LocalAuthorityCode.ShouldBe(stubbedOrganisationResponse.LocalAuthorityCode);
+        result.Status.ShouldBe(stubbedOrganisationResponse.Status.ToString());
+    }
+
+    [Fact]
+    public async Task Organisations_endpoint_should_return_ProblemDetails_for_invalid_laestab()
+    {
+        // Arrange
+        string invalidLaestab = "98765";
+
+
+        // Act
+        HttpResponseMessage response =
+            await _client.GetAsync($"/organisations/{invalidLaestab}", CancellationToken.None);
+
+        // Assert
+        response.StatusCode.ShouldBe(System.Net.HttpStatusCode.BadRequest);
+        ProblemDetails? problemDetails =
+            await response.Content.ReadFromJsonAsync<ProblemDetails>(CancellationToken.None);
+        problemDetails.ShouldNotBeNull();
+        problemDetails.Title.ShouldBe("One or more validation errors occurred.");
     }
 }
